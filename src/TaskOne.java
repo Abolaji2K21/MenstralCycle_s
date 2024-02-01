4103a13a670287f1c9ebcae416d18487eb84a7dc
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-02-01 06:04:54 +0100

    My phone book

--- src/TaskOne.java
+++
@@ -1,7 +0,0 @@
-public class TaskOne {
-    public static void main(String... args){
-        for(int check = 0; check < 10; check++){
-         System.out.println("Number # " + (check + 1));
-         }
-    }
-}
